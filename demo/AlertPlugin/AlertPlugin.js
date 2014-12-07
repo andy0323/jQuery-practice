f61705d5ea92a01e4fd73416adf8b5d1dc6c08f9
金健翔 <andy_ios@163.com>
2014-12-07 14:35:02 +0800

    add Demo_1

--- demo/AlertPlugin/AlertPlugin.js
+++ demo/AlertPlugin/AlertPlugin.js
@@ -1,24 +1,54 @@
 ;(function($) {   
 
-	$.fn.addTouchEventAlert = function(options) {  
+	/**
+	 *	处理事件
+	 */
+	function handleEvent(obj, opts) {
+		var type = opts.eventType;
 
-		var opts = $.extend({},{}, options);
+		if (type == 'alert') {
+			eventAlert(obj, opts.message);
+		}else if (type == 'log') {
+			eventLog(obj, opts.message);
+		}
+	}
 
-		this.click(function(){
-			alert(1);
+	/**
+	 *	警告框
+	 */
+	function eventAlert(obj, message) {
+		obj.click(function() {
+			alert(message);
 		});
-		
-		// this.alert = function() {
-		// 	this.each(function (index, dom) {
-		// 		this.click(function(){
-		// 			alert(1);
-		// 		});
-		// 	});
-		// }
+	}
+
+	/**
+	 *	控制台打印
+	 */
+	function eventLog(obj, message) {
+		obj.click(function() {
+			console.log(message);
+		});
+	}
+
+	/**
+	 *	添加一个警告框事件
+	 */
+	$.fn.onClickEventAlert = function(options) {  
+		// 获取参数
+		var opts = $.extend({}, $.fn.onClickEventAlert.defaults, options);
 
 		return this.each(function() { 
+			// 获取对象
+			var obj = $(this);
 
+			// 添加警告框事件
+			handleEvent(obj, opts);
 		});
-	};
-    
+	}
+    $.fn.onClickEventAlert.defaults = {
+    	eventType : 'alert',
+    	message   : '插件尚未传入参数'
+    };
+
 })(jQuery);
